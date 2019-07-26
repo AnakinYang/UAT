ecdc7815a74d26feb4077bf0d76fb17492798daa
Nikana <39976736+AnakinYang@users.noreply.github.com>
2019-07-27 00:17:48 +0800

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -11,19 +11,21 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 echo "set yum source"
 
 # set yum
-sed -i 's|plugins=1|plugins=0|' /etc/yum.conf
-sed -i 's|enabled=1|enabled=0|' /etc/yum/pluginconf.d/fastestmirror.conf
-yum clean dbcache
+# sed -i 's|plugins=1|plugins=0|' /etc/yum.conf
+# sed -i 's|enabled=1|enabled=0|' /etc/yum/pluginconf.d/fastestmirror.conf
+# yum clean dbcache
 
 
-curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
-echo "Set Docker source successful"
+
 
 yum –y install docker-ce-18.06.2.ce
+sleep 20
 systemctl start docker
 systemctl enable docker
 systemctl status docker | grep "active"
 echo "The active means docker install complete"
 
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
+echo "Set Docker source successful"
 
 echo "Congratulations, this host has successfully deployed docker."
