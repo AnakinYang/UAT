9680a99f03ae086b51b0b1ebfdca0ffce9d4cef3
Nikana <39976736+AnakinYang@users.noreply.github.com>
2019-07-27 00:59:48 +0800

    Create k8s.sh

---
+++ k8s.sh
@@ -0,0 +1,66 @@
+#! /bin/bash
+systemctl stop firewalld
+systemctl disable firewalld
+
+swapoff -a 
+sed -i 's/.*swap.*/#&/' /etc/fstab
+
+modprobe br_netfilter
+
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sysctl -p /etc/sysctl.d/k8s.conf
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.huaweicloud.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://mirrors.huaweicloud.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.huaweicloud.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+
+yum -y install epel-release
+yum -y install yum-utils device-mapper-persistent-data lvm2 net-tools  conntrack-tools wget vim  ntpdate libseccomp libtool-ltdl
+
+systemctl enable ntpdate.service
+echo '*/30 * * * * /usr/sbin/ntpdate time7.aliyun.com >/dev/null 2>&1' > /tmp/crontab2.tmp
+crontab /tmp/crontab2.tmp
+systemctl start ntpdate.service
+
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+# yum –y install docker-ce-18.06.2.ce
+curl -fsSL get.docker.com -o get-docker.sh
+sudo sh get-docker.sh --mirror Aliyun
+
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
+
+systemctl daemon-reload
+systemctl restart docker
+systemctl enable docker
+systemctl status docker
+
+images=(
+    kube-apiserver:v1.14.1
+    kube-controller-manager:v1.14.1
+    kube-scheduler:v1.14.1
+    kube-proxy:v1.14.1
+    pause:3.1
+    etcd:3.3.10
+    coredns:1.3.1
+)
+
+for imageName in ${images[@]} ; do
+    docker pull oceancloud/$imageName
+    docker tag oceancloud/$imageName k8s.gcr.io/$imageName
+done
+
+yum install -y kubelet-1.14.1-0.x86_64
+yum install -y kubeadm-1.14.1-0.x86_64
+yum install -y kubectl-1.14.1-0.x86_64
+systemctl enable kubelet
+
