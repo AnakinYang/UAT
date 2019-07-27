37602b79ca38428dcbdde939d37f3be67b57e12e
Nikana <39976736+AnakinYang@users.noreply.github.com>
2019-07-27 16:10:23 +0800

    Update and rename set_hostname to build_cluster.sh

--- build_cluster.sh
+++ build_cluster.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# set hostname and hostmap
+# set hostname,hostmap and build cluster
 
 echo -e " k8s-master \n k8s-node1 \n k8s-node2"
 read -p "Choose your host name: " name
@@ -11,4 +11,12 @@ echo -e "192.168.137.151 k8s-master\n192.168.137.152 k8s-node1\n192.168.137.153 
 
 hostname
 tail -n 3 /etc/hosts
+
+echo "hostname and hostmap Set Successful"
+echo "Start pull k8s.abt, Please enter <password> after <yes>"
+
+cd ;scp root@192.168.137.151:/root/k8s.abt .
+tail –n 1 k8s.abt | sh
+
+kubectl get node;kubectl get pods --namespace=kube-system
 echo "Set Successful"
