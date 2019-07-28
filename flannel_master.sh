4357fd9f54541b32eb504cb3347a5dec858b2c70
Nikana <39976736+AnakinYang@users.noreply.github.com>
2019-07-28 14:12:52 +0800

    Create flannel_master.sh

---
+++ flannel_master.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+(kubectl get node | tee before;kubectl get pods --namespace=kube-system) | tee before 
+
+cd ;wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f ~/kube-flannel.yml
+
+(kubectl get node | tee before;kubectl get pods --namespace=kube-system) | tee after
+
+echo "Wait a moment"
+sleep 100
+
+diff -y before after
+
+echo "> Flannel: The installation is complete <"
